8e6a47ca62865467c17d6bc8efc8487963af5736
shawn stenhouse <s_stenh@shawns-MacBook-Pro.local>
2016-01-19 16:25:50 -0500

    version1

--- js/components/App.js
+++ js/components/App.js
@@ -4,8 +4,6 @@ var App = React.createClass({
   render() {
     return (
       <div>
-        <h1>GitHub sample app!</h1>
-        <hr/>
         {this.props.children}
       </div>
     )
--- js/main.js
+++ js/main.js
@@ -6,15 +6,12 @@ import { createHistory } from 'history';
 import App from './components/App';
 import NotFound from './components/NotFound';
 import Home from './components/Home';
-import Followers from './components/Followers';
-import Repos from './components/Repos';
+
 
 var routes = (
   <Router history={createHistory()}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
-      <Route path="followers" component={Followers}/>
-      <Route path="repos" component={Repos}/>
       <Route path="*" component={NotFound}/>
     </Route>
   </Router>
